c35e313b8780fc2fbb7f94aafb779beaddfde0d2
Tobias Valbjørn <tvalbjoern@gmail.com>
2018-10-11 10:39:36 +0200

    Added spinner as well as specific on click listener for the spinner.

--- app/src/main/java/dk/tobias/adapterviews/arrayadapterapplication/MainActivity.kt
+++ app/src/main/java/dk/tobias/adapterviews/arrayadapterapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package dk.tobias.adapterviews.arrayadapterapplication
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,9 +20,26 @@ class MainActivity : AppCompatActivity() {
         val animals:Array<String> = resources.getStringArray(R.array.animals)
         val adapter= ArrayAdapter(this,R.layout.list_item,animals)
         main_list_view.adapter=adapter
-        main_list_view.setOnItemClickListener { adapterView: AdapterView<*>?, view: View?, pos: Int, id: Long ->
-            val text: String=adapterView?.getItemAtPosition(pos) as String
-            Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+
+        //on click listener for spinner
+        main_list_view?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?,
+                                        pos: Int, id: Long) {
+                val text:String=adapterView?.getItemAtPosition(pos) as String
+                Toast.makeText(this@MainActivity,text,Toast.LENGTH_SHORT).show()
+            }
+
         }
+
+        //This listener works for both gridView and ListView, but not for spinner
+        //main_list_view.setOnItemClickListener { adapterView: AdapterView<*>?,
+           //                                     view: View?, pos: Int, id: Long ->
+            //val text: String=adapterView?.getItemAtPosition(pos) as String
+            //Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+        //}
     }
 }
